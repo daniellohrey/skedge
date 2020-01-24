aa6652188cb2ed37178b2dabd61c9364fcb91af3
daniiellohrey <daniel.b.lohrey@gmail.com>
2020-01-24 17:58:22 +1100

    debugging, refoactoring and testing

--- backend/skedge/views.py
+++ backend/skedge/views.py
@@ -82,10 +82,8 @@ def schedule(request):
 	data['days'] = headers
 	return render(request, 'skedge/schedule.html', data)
 
-av = "B"
 @login_required
 def generate_schedule(request):
-	global av
 	if request.method == 'POST':
 		try:
 			p = Schedule_Parameters.objects.all()[0]
@@ -96,240 +94,256 @@ def generate_schedule(request):
 			form.save()
 		else:
 			return HttpResponseRedirect("/")
+		status = generate()
+		if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+			return HttpResponseRedirect('/schedule/')
+		else:
+			return HttpResponseRedirect('/?error=1')
+	else:
+		return HttpResponseRedirect('/')
 
-		employees = []
-		for employee in Employee.objects.all():
-			employees.append(employee.availability)
-		params = Schedule_Parameters.objects.all()[0]
-		num_employees = len(employees)
-		num_days = 5
-		num_shifts = 2
 
-		#penalties should be such that you rather have someone work more than ideal than be one person short for a shift
-		COVER_PENALTY = 10
-		WEEKLY_SUM_PENALTY = 1
+av = "B"
+def generate():
+	global av
+	employees = []
+	for employee in Employee.objects.all():
+		employees.append(employee.availability)
+	params = Schedule_Parameters.objects.all()[0]
+	num_employees = len(employees)
+	num_days = 5
+	num_shifts = 2
 
-		int_vars = []
-		int_coeffs = []
-		model = cp_model.CpModel()
+	#rather have people work more than ideal than be 1 short
+	COVER_PENALTY = 10
+	WEEKLY_SUM_PENALTY = 1
 
-		#create morning/afternoon shift for each employee/day
-		work = {}
-		for e in range(num_employees):
-			for d in range(num_days):
-				for s in range(num_shifts):
-					work[e, d, s] = model.NewBoolVar('work%i_%i_%i' % (e, d, s))
+	int_vars = []
+	int_coeffs = []
+	model = cp_model.CpModel()
 
-		#create dummy employee to find gaps
+	#create morning/afternoon shift for each employee/day
+	work = {}
+	for e in range(num_employees):
+		for d in range(num_days):
+			for s in range(num_shifts):
+				work[e, d, s] = model.NewBoolVar('work%i_%i_%i' % (e, d, s))
 
-		#set availabilites from 2-230
-		working_from_2 = [[], [], [], [], []] #for choosing workers to work from 2
-		available_from_2 = [[], [], [], [], []]
-		from_2 = {}
-		for e in range(num_employees):
-			for d in range(num_days):
-				w = employees[e]
-				from_2[e, d] = model.NewBoolVar('from_2_%i_%i' % (e, d))
-				key = "a" + str(d) + "_1"
-				exec("global av; av = w." + key)
-				if av == 'N':
-					#if not available set to not working
-					model.Add(from_2[e, d] == 0)
-				else:
-					#if available/working set to available
-					model.Add(from_2[e, d] == 1)
-					if av == 'W':
-						#check if working afternoon
-						key = "a" + str(d) + "_2"
-						exec("global av; av = w." + key)
-						if av != 'N':
-							#if working set working afternoon
-							model.Add(work[e, d, 1] == 1)
-							working_from_2[d].append(e)
-					else:
-						available_from_2[d].append(e)
-
-		#set availabilites from 530-6, see from_2
-		working_till_6 = [[], [], [], [], []]
-		available_till_6 = [[], [], [], [], []]
-		till_6 = {}
-		for e in range(num_employees):
-			for d in range(num_days):
-				w = employees[e]
-				till_6[e, d] = model.NewBoolVar('till_6_%i_%i' % (e, d))
-				key = "a" + str(d) + "_3"
-				exec("global av; av = w." + key)
-				if av == 'N':
-					model.Add(till_6[e, d] == 0)
-				else:
-					model.Add(till_6[e, d] == 1)
-					if av == 'W':
-						key = "a" + str(d) + "_2"
-						exec("global av; av = w." + key)
-						if av != 'N':
-								model.Add(work[e, d, 1] == 1)
-								working_till_6[d].append(e)
-					else:
-						available_till_6[d].append(e)
-				
-		#set working/not available shifts
-		for e in range(num_employees):
-			for d in range(num_days):
-				for s, s2 in [(0, 0), (1, 2)]:
-					w = employees[e]
-					key = "a" + str(d) + "_" + str(s2)
+	#set availabilites from 2-230
+	working_from_2 = [[], [], [], [], []] #for choosing workers to work from 2
+	available_from_2 = [[], [], [], [], []]
+	from_2 = {}
+	for e in range(num_employees):
+		for d in range(num_days):
+			w = employees[e]
+			from_2[e, d] = model.NewBoolVar('from_2_%i_%i' % (e, d))
+			key = "a" + str(d) + "_1"
+			exec("global av; av = w." + key)
+			if av == 'N':
+				#if not available set to not working
+				model.Add(from_2[e, d] == 0)
+			else:
+				#if available/working set to available
+				model.Add(from_2[e, d] == 1)
+				if av == 'W':
+					#check if working afternoon
+					key = "a" + str(d) + "_2"
 					exec("global av; av = w." + key)
-					if av == 'N':
-						model.Add(work[e, d, s] == 0)
-					elif av == 'W':
-						model.Add(work[e, d, s] == 1)
+					if av != 'N':
+						#if working set working afternoon
+						model.Add(work[e, d, 1] == 1)
+						working_from_2[d].append(e)
+				else:
+					available_from_2[d].append(e)
 
-		#add cover contraints
+	#set availabilites from 530-6, see from_2
+	working_till_6 = [[], [], [], [], []]
+	available_till_6 = [[], [], [], [], []]
+	till_6 = {}
+	for e in range(num_employees):
+		for d in range(num_days):
+			w = employees[e]
+			till_6[e, d] = model.NewBoolVar('till_6_%i_%i' % (e, d))
+			key = "a" + str(d) + "_3"
+			exec("global av; av = w." + key)
+			if av == 'N':
+				model.Add(till_6[e, d] == 0)
+			else:
+				model.Add(till_6[e, d] == 1)
+				if av == 'W':
+					key = "a" + str(d) + "_2"
+					exec("global av; av = w." + key)
+					if av != 'N':
+							model.Add(work[e, d, 1] == 1)
+							working_till_6[d].append(e)
+				else:
+					available_till_6[d].append(e)
+			
+	#set working/not available shifts
+	for e in range(num_employees):
 		for d in range(num_days):
 			for s, s2 in [(0, 0), (1, 2)]:
-				key = "p" + str(d) + "_" + str(s2)
-				exec("global av; av = params." + key)
-				working = model.NewIntVar(av - 1, av, "working%i_%i" % (d, s))
-				model.Add(sum(work[e, d, s] for e in range(num_employees)) == working)
-				ideal = model.NewIntVar(0, num_employees, "ideal%i_%i" % (d, s))
-				model.Add(ideal == av)
-				delta = model.NewIntVar(-1, 1, "delta%i_%i" % (d, s))
-				model.Add(delta == ideal - working)
-				delta_penalty = model.NewIntVar(0, 1, 'cover%i_%i' % (d, s))
-				model.AddAbsEquality(delta_penalty, delta)
-				int_vars.append(delta_penalty)
-				int_coeffs.append(COVER_PENALTY)
+				w = employees[e]
+				key = "a" + str(d) + "_" + str(s2)
+				exec("global av; av = w." + key)
+				if av == 'N':
+					model.Add(work[e, d, s] == 0)
+				elif av == 'W':
+					model.Add(work[e, d, s] == 1)
 
-		#add cover requirement from 2-230
-		from_2_check = {}
-		for e in range(num_employees):
-			for d in range(num_days):
-				from_2_check[e, d] = model.NewBoolVar('from_2_check%i_%i' % (e, d))
-				#from_2_check is only true when work/from_2 are true
-				model.Add(from_2_check[e, d] == 1).OnlyEnforceIf([work[e, d, 1], from_2[e, d]])
-				model.Add(from_2_check[e, d] == 0).OnlyEnforceIf(work[e, d, 1].Not())
-				model.Add(from_2_check[e, d] == 0).OnlyEnforceIf(from_2[e, d].Not())
-		for d in range(num_days):
-			key = "p" + str(d) + "_1"
+	#add cover contraints
+	for d in range(num_days):
+		for s, s2 in [(0, 0), (1, 2)]:
+			key = "p" + str(d) + "_" + str(s2)
 			exec("global av; av = params." + key)
-			model.Add(sum([from_2_check[e, d] for e in range(num_employees)]) >= av)
-
-		#add cover requirement from 530-6
-		till_6_check = {}
-		for e in range(num_employees):
-			for d in range(num_days):
-				till_6_check[e, d] = model.NewBoolVar('till_6_check%i_%i' % (e, d))
-				#from_2_check is only true when work/from_2 are true
-				model.Add(till_6_check[e, d] == 1).OnlyEnforceIf([work[e, d, 1], till_6[e, d]])
-				model.Add(till_6_check[e, d] == 0).OnlyEnforceIf(work[e, d, 1].Not())
-				model.Add(till_6_check[e, d] == 0).OnlyEnforceIf(till_6[e, d].Not())
-		for d in range(num_days):
-			key = "p" + str(d) + "_3"
-			exec("global av; av = params." + key)
-			model.Add(sum([from_2_check[e, d] for e in range(num_employees)]) >= av)
-
-		#add ideal number of shifts for each employee
-		for e in range(num_employees):
-			w = employees[e]
-			weekly_sum = model.NewIntVar(0, 10, "weekly_sum%i" % (e))
-			model.Add(sum(work[e, d, s] for d in range(num_days) for s in range(num_shifts)) == weekly_sum)
-			ideal = model.NewIntVar(0, 10, "weekly_ideal%i" % (e))
-			model.Add(ideal == w.weekly_ideal)
-			delta = model.NewIntVar(-10, 10, "weekly_delta%i" % (e))
-			model.Add(delta == ideal - weekly_sum)
-			delta_penalty = model.NewIntVar(0, 10, 'weekly%i' % (e))
+			working = model.NewIntVar(av - 1, av, "working%i_%i" % (d, s))
+			model.Add(sum(work[e, d, s] for e in range(num_employees)) == working)
+			ideal = model.NewIntVar(0, num_employees, "ideal%i_%i" % (d, s))
+			model.Add(ideal == av)
+			delta = model.NewIntVar(-1, 1, "delta%i_%i" % (d, s))
+			model.Add(delta == ideal - working)
+			delta_penalty = model.NewIntVar(0, 1, 'cover%i_%i' % (d, s))
 			model.AddAbsEquality(delta_penalty, delta)
 			int_vars.append(delta_penalty)
-			int_coeffs.append(WEEKLY_SUM_PENALTY)
+			int_coeffs.append(COVER_PENALTY)
 
-		#set objective
-		model.Minimize(sum(int_vars[i] * int_coeffs[i] for i in range(len(int_vars))))
+	#add cover requirement from 2-230
+	from_2_check = {}
+	for e in range(num_employees):
+		for d in range(num_days):
+			from_2_check[e, d] = model.NewBoolVar('from_2_check%i_%i' % (e, d))
+			#from_2_check is only true when work/from_2 are true
+			model.Add(from_2_check[e, d] == 1).OnlyEnforceIf([work[e, d, 1], from_2[e, d]])
+			model.Add(from_2_check[e, d] == 0).OnlyEnforceIf(work[e, d, 1].Not())
+			model.Add(from_2_check[e, d] == 0).OnlyEnforceIf(from_2[e, d].Not())
+	for d in range(num_days):
+		key = "p" + str(d) + "_1"
+		exec("global av; av = params." + key)
+		model.Add(sum([from_2_check[e, d] for e in range(num_employees)]) >= av)
 
-		#solve model
-		solver = cp_model.CpSolver()
-		solver.parameters.num_search_workers = 8 #adjust for production server
-		#printer = cp_model.ObjectiveSolutionPrinter()
-		#status = solver.SolveWithSolutionCallback(model, printer)
-		status = solver.Solve(model)
+	#add cover requirement from 530-6
+	till_6_check = {}
+	for e in range(num_employees):
+		for d in range(num_days):
+			till_6_check[e, d] = model.NewBoolVar('till_6_check%i_%i' % (e, d))
+			#from_2_check is only true when work/from_2 are true
+			model.Add(till_6_check[e, d] == 1).OnlyEnforceIf([work[e, d, 1], till_6[e, d]])
+			model.Add(till_6_check[e, d] == 0).OnlyEnforceIf(work[e, d, 1].Not())
+			model.Add(till_6_check[e, d] == 0).OnlyEnforceIf(till_6[e, d].Not())
+	for d in range(num_days):
+		key = "p" + str(d) + "_3"
+		exec("global av; av = params." + key)
+		model.Add(sum([till_6_check[e, d] for e in range(num_employees)]) >= av)
 
-		if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-			employee_schedules = []
-			for employee in Employee.objects.all():
-				employee_schedules.append(employee.schedule)
+	#add ideal number of shifts for each employee
+	for e in range(num_employees):
+		w = employees[e]
+		weekly_sum = model.NewIntVar(0, 10, "weekly_sum%i" % (e))
+		model.Add(sum(work[e, d, s] for d in range(num_days) for s in range(num_shifts)) == weekly_sum)
+		ideal = model.NewIntVar(0, 10, "weekly_ideal%i" % (e))
+		model.Add(ideal == w.weekly_ideal)
+		half_ideal = model.NewIntVar(0, 10, "half_ideal%i" % (e))
+		model.AddDivisionEquality(half_ideal, ideal, 2)
+		ideal_3 = model.NewIntVar(0, 10, "ideal_3_%i" % (e))
+		model.Add(ideal_3 == ideal - 3)
+		min_ideal = model.NewIntVar(0, 10, "min_ideal%i" % (e))
+		model.AddMaxEquality(min_ideal, [half_ideal, ideal_3])
+		model.Add(weekly_sum > min_ideal)
+		#add calculate max_ideal
+		#model.Add(weekly_sum <= max_ideal)
+		delta = model.NewIntVar(-10, 10, "weekly_delta%i" % (e))
+		model.Add(delta == ideal - weekly_sum)
+		delta_penalty = model.NewIntVar(0, 10, 'weekly%i' % (e))
+		model.AddAbsEquality(delta_penalty, delta)
+		int_vars.append(delta_penalty)
+		int_coeffs.append(WEEKLY_SUM_PENALTY)
 
-			#set employees schedules
-			for e in range(num_employees):
-				w = employee_schedules[e]
-				for d in range(num_days):
-					#set whether working morning/afternoon
-					for s, s2 in [(0, 0), (1, 2)]:
-						key = "w" + str(d) + "_" + str(s2)
-						av = solver.BooleanValue(work[e, d, s])
-						if av:
-							exec("w." + key + " = 'W'")
-						else:
-							exec("w." + key + " = 'N'")
+	#set objective
+	model.Minimize(sum(int_vars[i] * int_coeffs[i] for i in range(len(int_vars))))
 
-					#set all not working from 2/till 6
-					for s2 in [1, 3]:
-						key = "w" + str(d) + "_" + str(s2)
+	#solve model
+	solver = cp_model.CpSolver()
+	#solver.parameters.num_search_workers = 8 #adjust for production server
+	#printer = cp_model.ObjectiveSolutionPrinter()
+	#status = solver.SolveWithSolutionCallback(model, printer)
+	status = solver.Solve(model)
+
+	if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+		employee_schedules = []
+		for employee in Employee.objects.all():
+			employee_schedules.append(employee.schedule)
+
+		#set employees schedules
+		for e in range(num_employees):
+			w = employee_schedules[e]
+			for d in range(num_days):
+				#set whether working morning/afternoon
+				for s, s2 in [(0, 0), (1, 2)]:
+					key = "w" + str(d) + "_" + str(s2)
+					av = solver.BooleanValue(work[e, d, s])
+					if av:
+						exec("w." + key + " = 'W'")
+					else:
 						exec("w." + key + " = 'N'")
-				#w.save()
 
-			random.seed()
-			for d in range(5):
-				for e in working_from_2[d]:
-						w = employee_schedules[e]
-						key = "w" + str(d) + "_1"
-						exec("w." + key + " = 'W'")
-						#w.save()
+				#set all not working from 2/till 6
+				for s2 in [1, 3]:
+					key = "w" + str(d) + "_" + str(s2)
+					exec("w." + key + " = 'N'")
+			#w.save()
 
-				for e in working_till_6[d]:
-						w = employee_schedules[e]
-						key = "w" + str(d) + "_3"
-						exec("w." + key + " = 'W'")
-						#w.save()
+		#set employees working from 2/till 6
+		random.seed()
+		for d in range(5):
+			for e in working_from_2[d]:
+					w = employee_schedules[e]
+					key = "w" + str(d) + "_1"
+					exec("w." + key + " = 'W'")
+					#w.save()
 
-				key = "p" + str(d) + "_1"
-				exec("global av; av = params." + key)
-				if len(working_from_2[d]) < av:
-					delta = av - len(working_from_2[d])
-					available = []
-					for e in available_from_2[d]:
-						w = employee_schedules[e]
-						key = "w" + str(d) + "_2"
-						exec("global av; av = w." + key)
-						if av == 'W':
-							available.append(e)
-					working = random.sample(available, delta)
-					for e in working:
-						w = employee_schedules[e]
-						key = "w" + str(d) + "_1"
-						exec("w." + key + " = 'W'")
-						#w.save()
+			for e in working_till_6[d]:
+					w = employee_schedules[e]
+					key = "w" + str(d) + "_3"
+					exec("w." + key + " = 'W'")
+					#w.save()
 
-				key = "p" + str(d) + "_3"
-				exec("global av; av = params." + key)
-				if len(working_till_6[d]) < av:
-					delta = av - len(working_till_6[d])
-					available = []
-					for e in available_till_6[d]:
-						w = employee_schedules[e]
-						key = "w" + str(d) + "_2"
-						exec("global av; av = w." + key)
-						if av == 'W':
-							available.append(e)
-					working = random.sample(available, delta)
-					for e in working:
-						w = employee_schedules[e]
-						key = "w" + str(d) + "_3"
-						exec("w." + key + " = 'W'")
-						#w.save()
+			#choose from those available to start at 2
+			key = "p" + str(d) + "_1"
+			exec("global av; av = params." + key)
+			if len(working_from_2[d]) < av:
+				delta = av - len(working_from_2[d])
+				available = []
+				for e in available_from_2[d]:
+					w = employee_schedules[e]
+					key = "w" + str(d) + "_2"
+					exec("global av; av = w." + key)
+					if av == 'W':
+						available.append(e)
+				working = random.sample(available, delta)
+				for e in working:
+					w = employee_schedules[e]
+					key = "w" + str(d) + "_1"
+					exec("w." + key + " = 'W'")
+					#w.save()
 
-			for e in employee_schedules:
-				e.save()
+			key = "p" + str(d) + "_3"
+			exec("global av; av = params." + key)
+			if len(working_till_6[d]) < av:
+				delta = av - len(working_till_6[d])
+				available = []
+				for e in available_till_6[d]:
+					w = employee_schedules[e]
+					key = "w" + str(d) + "_2"
+					exec("global av; av = w." + key)
+					if av == 'W':
+						available.append(e)
+				working = random.sample(available, delta)
+				for e in working:
+					w = employee_schedules[e]
+					key = "w" + str(d) + "_3"
+					exec("w." + key + " = 'W'")
+					#w.save()
 
-			return HttpResponseRedirect('/schedule/')
-
-	return HttpResponseRedirect('/?error=1')
-
+		for e in employee_schedules:
+			e.save()
+	return status
